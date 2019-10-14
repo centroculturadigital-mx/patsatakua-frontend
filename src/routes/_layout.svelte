bc14b719b30b615a9d7ce28c384b0ba4355442cb
josecaos <jazzvoon@gmail.com>
2019-10-14 16:22:07 -0500

    Acomodo barra lateral + tests particulas(dependencia rota aun buscando la mejor ocion)

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,14 +1,41 @@
 <script>
+import { onMount } from 'svelte' 
+
+onMount(async () => {
+
+	// const { default: Particles } = await import(`../../node_modules/particlesjs/src/particles.js`);	
+	const { default: Particles } = await import(`../../node_modules/particlesjs/dist/particles.js`);	
+	// console.log(Particles)
+
+	Particles.init({
+		selector: '#FondoParticulas',
+	})
+
+})
+
+
 </script>
 
 <style>
 
 	@import url('https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500,600,700&display=swap');
-
+	main {
+		position: relative;
+	}
+	#FondoParticulas {
+		position: absolute;
+		top: 0;
+		left: 0;
+		height: 100vh;
+		width: 100vw;
+		z-index: -1;
+	}
 
 </style>
 
 <main>
+
+	<div id="FondoParticulas"></div>
 
 	<slot></slot>
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,173 +1,171 @@
 <script>
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
 
-	import { onMount } from 'svelte'
-	import { fade } from 'svelte/transition';
+  import ProyectoVistaPrevia from "../componentes/ProyectoVistaPrevia.svelte";
+  import Pie from "../componentes/Pie.svelte";
 
-	import ProyectoVistaPrevia from "../componentes/ProyectoVistaPrevia.svelte";
-	import Pie from "../componentes/Pie.svelte";
+  let proyectosModule;
+  $: proyectos =
+    proyectosModule && proyectosModule.default ? proyectosModule.default : [];
+  let mostrarLateral = false;
 
-	let proyectosModule
-	$: proyectos = proyectosModule && proyectosModule.default ? proyectosModule.default : [] 
-	let mostrarLateral = false
+  setTimeout(() => (mostrarLateral = true), 600);
 
-	setTimeout(()=>mostrarLateral=true,600)
-
-	onMount(async () => {
-			proyectosModule = await import('../datos/proyectos.json')
-			console.log(proyectosModule)
-	})
-
+  onMount(async () => {
+    proyectosModule = await import("../datos/proyectos.json");
+    console.log(proyectosModule);
+  });
 </script>
 
-
 <style>
+  @media screen and (min-width: 768px) {
+    .Inicio {
+      min-height: 100%;
+      display: grid;
+      grid-template-columns: 1fr 2fr;
+    }
+  }
 
-	@media screen and (min-width: 768px ) {
-		
-		.Inicio {
-			min-height: 100%;
-			display: grid;
-			grid-template-columns: 1fr 2fr;
-		}
+  .Logotipo {
+    padding: 0;
+    width: 12rem;
+    height: 100%;
+    object-fit: contain;
+    object-position: 0 0;
+  }
 
-	}
-
-		
-	.Logotipo {
-    	padding: 0;
-    	width: 12rem;
-    	height: 100%;
-    	object-fit: contain;
-    	object-position: 0 0;
-	}
-
-	/* .contenedor-principal {
+  /* .contenedor-principal {
 	} */
-	.Titulo {
-		color: #72A6AA;
-		font-weight: 500;
-		font-size: 28px;
+  .Titulo {
+    color: #72a6aa;
+    font-weight: 500;
+    font-size: 28px;
+    /* text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black; */
+  }
 
-	}
-	
-	.Lateral {
-		color: #000;
-		height: 100vh;
-		max-width: 260px;
-        background-color: rgb(255, 255, 255);
-		padding: 2rem;
-	}
+  .Lateral {
+    color: #000;
+    height: 100vh;
+    background-color: rgb(255, 255, 255);
+    padding-top: 2rem;
+    display: flex;
+    justify-content: center;
+  }
 
-	.Introduccion {
-		font-weight: 200;
-		font-size: 14px;
-		line-height: 17px;
-	}
+  .LateralContenido {
+    max-width: 260px;
+  }
 
-	.Subtitulo {
-		font-weight: 500;
-		font-size: 12px;
-		line-height: 14px;
-	}
+  .Introduccion {
+    font-weight: 200;
+    font-size: 14px;
+    line-height: 17px;
+  }
 
-	.Creditos {
-		font-weight: 200;
-		font-size: 0.8rem;
-		line-height: 14px;
-		color:#454344;
-		text-align: center;
-	}
+  .Subtitulo {
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+  }
 
-	.contenedor-principal {
-		height: 100vh;
-		overflow-y: auto;
-		overflow-x: hidden;
-		padding: 0;
-	}
+  .Creditos {
+    font-weight: 200;
+    font-size: 0.8rem;
+    line-height: 14px;
+    color: #454344;
+    text-align: center;
+  }
 
-	
+  .contenedor-principal {
+    height: 100vh;
+    overflow-y: auto;
+    overflow-x: hidden;
+    padding: 0;
+    padding-top: 2rem;
+  }
 
-	@media screen and (min-width: 768px ) {
-	
-		.Lista, .Lista ul {
-			height: auto;
-			padding-bottom: 5%;
-		}
+  @media screen and (min-width: 768px) {
+    .Lista,
+    .Lista ul {
+      height: auto;
+      padding-bottom: 5%;
+    }
 
-		.Lista ul {
-			padding: 0 1rem;
-			display: grid;
-			grid-template-columns: 1fr 1fr;
-			grid-gap: 2rem;
-		}
+    .Lista ul {
+      padding: 0 1rem;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: 2rem;
+    }
+  }
 
-	}
-
-	@media screen and (min-width: 1200px ){
-		
-		.Lista ul {
-			grid-template-columns: 1fr 1fr 1fr;
-		}
-
-	}
-
+  @media screen and (min-width: 1200px) {
+    .Lista ul {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
 </style>
 
-	<svelte:head>
-		<title>Patsatakua</title>
-	</svelte:head>
-
+<svelte:head>
+  <title>Patsatakua</title>
+</svelte:head>
 
 <section class="Inicio">
-	<div class="Lateral">
-		{#if mostrarLateral}
-			<header>
-				<h1 class="Titulo" transition:fade>
-					Patsatakua
-				</h1>
+  <div class="Lateral">
 
-				<h4 class="Subtitulo" transition:fade>
-					Purépecha (P’urhépecha) que quiere decir, “en lo que se guarda”
-				</h4>
-				<p class="Subtitulo" transition:fade>Cómo se pronuncia ‘patsatakua’:/pa.tsá.ta.kwa/</p>
-			</header>
+    <div class="LateralContenido">
 
+      {#if mostrarLateral}
+        <header>
+          <h1 class="Titulo" transition:fade>Patsatakua</h1>
 
-			<section class="Introduccion" transition:fade>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur labore molestias dicta ex veritatis accusantium ullam impedit praesentium, nulla, nesciunt vero esse quo. Dolores fugiat, distinctio expedita iste debitis excepturi.
-				</p>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias nihil aut pariatur voluptatum maiores necessitatibus explicabo totam vero ad sit ex, eligendi ipsum, dignissimos saepe consequatur error officiis optio accusantium.
-				</p>
-			
-				<!-- <img class="Logotipo" src="http://fakeimg.pl/244x80?text=logotipos" alt="img" /> -->
+          <h4 class="Subtitulo" transition:fade>
+            Purépecha (P’urhépecha) que quiere decir, “en lo que se guarda”
+          </h4>
+          <p class="Subtitulo" transition:fade>
+            Cómo se pronuncia ‘patsatakua’:/pa.tsá.ta.kwa/
+          </p>
+        </header>
 
-			</section>
+        <section class="Introduccion" transition:fade>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+            labore molestias dicta ex veritatis accusantium ullam impedit
+            praesentium, nulla, nesciunt vero esse quo. Dolores fugiat,
+            distinctio expedita iste debitis excepturi.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+            nihil aut pariatur voluptatum maiores necessitatibus explicabo totam
+            vero ad sit ex, eligendi ipsum, dignissimos saepe consequatur error
+            officiis optio accusantium.
+          </p>
 
-			<section>
-				<h5 class="Creditos">
-					Créditos proyectos
-				</h5>
-			</section>
+          <!-- <img class="Logotipo" src="http://fakeimg.pl/244x80?text=logotipos" alt="img" /> -->
 
+        </section>
 
-		{/if}
-	</div>
+        <section>
+          <h5 class="Creditos">Créditos proyectos</h5>
+        </section>
+      {/if}
 
-	<div class="contenedor-principal">
+    </div>
+  </div>
 
-		<section class="Lista">
-			<ul>
-				{#each proyectos as p}
-					<ProyectoVistaPrevia proyecto={p}/>				
-				{/each}
-			</ul>
-		</section>
+  <div class="contenedor-principal">
 
+    <section class="Lista">
+      <ul>
+        {#each proyectos as p}
+          <ProyectoVistaPrevia proyecto={p} />
+        {/each}
+      </ul>
+    </section>
 
-	<Pie/>
+    <Pie />
 
-</div>
+  </div>
 
 </section>
