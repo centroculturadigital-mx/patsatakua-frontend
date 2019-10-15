f3d5752a2f675924199ad73cd885c693cd6d8e9d
josecaos <jazzvoon@gmail.com>
2019-10-14 20:37:08 -0500

    En proceso: proyectos similares en vista proyecto

---
+++ src/componentes/ProyectosSimilares.svelte
@@ -0,0 +1,26 @@
+<script></script>
+<style>
+section {
+    background-color: transparent; 
+}
+
+#ProyectosSimilares {
+
+}
+
+</style>
+<!--  -->
+
+<section>
+
+<h2>ProyectosSimilares</h2>
+
+<div id="ProyectosSimilares">
+
+{#each Array(4) as _}
+    {console.log("4 Veces")}
+{/each}
+
+</div>
+
+</section>
--- src/routes/proyectos/[id].svelte
+++ src/routes/proyectos/[id].svelte
@@ -1,195 +1,179 @@
-
 <script context="module">
-	export async function preload(page, session) {
-        const { id } = page.params
-        console.log('id', id)
-        return { id }
-	}
+  export async function preload(page, session) {
+    const { id } = page.params;
+    console.log("id", id);
+    return { id };
+  }
 </script>
+
 <script>
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
 
-    import { onMount } from 'svelte'
-	import { fade } from 'svelte/transition';
+  import Pie from "../../componentes/Pie.svelte";
+  import Boton from "../../componentes/elementos/Boton.svelte";
+  //   import ProyectoVistaPrevia from "../../componentes/ProyectoVistaPrevia.svelte";
+  import ProyectosSimilares from "../../componentes/ProyectosSimilares.svelte";
 
-	import Pie from "../../componentes/Pie.svelte";
-	import Boton from "../../componentes/elementos/Boton.svelte";
-    import ProyectoVistaPrevia from "../../componentes/ProyectoVistaPrevia.svelte";
-    //import Fa from "../../../../patsatakua-frontend/node_modules/svelte-fa/dist/svelte-fa.mjs"
-    export let id
+  export let id;
 
-	let proyectosModule
-    $: proyecto = proyectosModule && proyectosModule.default ?
-        proyectosModule.default.find(p => {
-            proyectosModule.default
-            return p.id === id
-        }) : {} 
+  let proyectosModule;
+  $: proyecto =
+    proyectosModule && proyectosModule.default
+      ? proyectosModule.default.find(p => {
+          proyectosModule.default;
+          return p.id === id;
+        })
+      : {};
 
-	onMount(async () => {
-			proyectosModule = await import('../../datos/proyectos.json')
-			console.log('aa', proyectosModule.default, id)
-	})
+  onMount(async () => {
+    proyectosModule = await import("../../datos/proyectos.json");
+    console.log("aa", proyectosModule.default, id);
+  });
 
-    $: console.log('proyecto', proyecto)
-    $: console.log('id', id)
+  $: console.log("proyecto", proyecto);
+  $: console.log("id", id);
 </script>
 
 <style>
-    @media screen and (min-width:768px) {
-
-        .ProyectoDetalle {
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            background: #FFFFFF;
-            border: 0.5px solid #72A6AA;
-            box-sizing: border-box;  
-            max-width: 800px;
-            padding: 3%;  
-        }
-
-        .Imagen {
-            width: 100%;
-            height: 20rem;
-            object-fit: cover;
-        }
-
-        .ProyectoDetalle + section ul,
-        .ProyectoDetalle + section + section ul {
-            display: flex;
-            flex-direction: row !important;
-            justify-content: space-evenly;
-            flex-wrap: wrap;
-            height: auto;
-        }
-
+  @media screen and (min-width: 768px) {
+    .ProyectoDetalle {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      background: #ffffff;
+      border: 0.5px solid #72a6aa;
+      box-sizing: border-box;
+      max-width: 800px;
+      padding: 3%;
     }
-
-    .contenedor-1 {
-        padding: 0;
+    .Imagen {
+      width: 100%;
+      height: 20rem;
+      object-fit: cover;
     }
-    
-    .contenedor-1 header {
-        position: relative;
+    .ProyectoDetalle + section ul,
+    .ProyectoDetalle + section + section ul {
+      display: flex;
+      flex-direction: row !important;
+      justify-content: space-evenly;
+      flex-wrap: wrap;
+      height: auto;
     }
-
-    .contenedor-1 .Regresar {
-        /*margin-left: -4rem;*/
-        font-weight: 100;
-    }
-    
-    .contenedor-2 footer {
-        margin-top: 1rem;
-    }
-
-    footer div {
-        display: flex;
-        width: 100%;
-        justify-content: space-evenly;
-    }
-    footer article {
-        width: auto;
-        padding: 3rem;
-    }
-
-    .Subtitulo {
-        text-align: center;
-    }
-
-    .Titulo {
-        font-weight: bold;
-        font-size: 40px;
-        line-height: 48px;
-    }
-
-    .Contenido {
-        width: 80%;
-        font-weight: 200;
-        font-size: 16px;
-        line-height: 22px;
-        left: 15px;
-       /* margin: 25px;*/
-
-    }
-     
-    .Proyecto {
-       /* left: 23px;*/
-    }
-
-    ul {
-        margin-bottom: 30px;
-        margin-top: 25px;
-    }
-    
-    section {
-        margin: 0;
-    }
-
-    .SubProyectos {
-        margin-top: 3em;
-    }
-
-    .Similares {
-        margin-top: 3em;
-    }
-
-    .fa {
+  }
+  .contenedor-1 {
+    padding: 0;
+  }
+  .contenedor-1 header {
+    position: relative;
+  }
+  .contenedor-1 .Regresar {
+    /*margin-left: -4rem;*/
+    font-weight: 100;
+  }
+  .contenedor-2 footer {
+    margin-top: 1rem;
+  }
+  footer div {
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+  }
+  footer article {
+    width: auto;
+    padding: 3rem;
+  }
+  .Subtitulo {
+    text-align: center;
+  }
+  .Titulo {
+    font-weight: bold;
+    font-size: 40px;
+    line-height: 48px;
+  }
+  .Contenido {
+    width: 80%;
+    font-weight: 200;
+    font-size: 16px;
+    line-height: 22px;
+    left: 15px;
+    /* margin: 25px;*/
+  }
+  .Proyecto {
+    /* left: 23px;*/
+  }
+  ul {
+    margin-bottom: 30px;
+    margin-top: 25px;
+  }
+  section {
+    margin: 0;
+  }
+  .SubProyectos {
+    margin-top: 3em;
+  }
+  .Similares {
+    margin-top: 3em;
+  }
+  .fa {
     font-family: "Font Awesome 5 Free";
     font-weight: 200;
     font-style: normal;
-    }
-
-     .fa-home {
+  }
+  .fa-home {
     font-family: "Font Awesome 5 Free";
     font-size: 2rem;
     font-style: normal;
-    color:#72A6AA;
-    }
+    color: #72a6aa;
+  }
+  /*  */
+  .RegresaInicio {
+    position: absolute;
+    left: 1.5rem;
+    top: 1.5rem;
+    border: 1px solid #72a6aa;
+    padding: 1rem;
+  }
 
-
-
+  /*  */
 </style>
 
-<header>
-    <a href="/">
-      <i class="fa fa-home"></i>                               
-   </a>
-</header>
+<a class="RegresaInicio" href="/">
+  <i class="fa fa-home" />
+</a>
 <article class="ProyectoDetalle ancho_maximo">
-    <div class="contenedor-1">  
-        <header class="Proyecto">
-            <a class="Regresar" href="/">
-                <Boton>
-                    <i class="fa fa-arrow-left"></i>
-                    <span>
-                        Volver
-                    </span>
-                </Boton>
-            </a>
-            <h1 class="Titulo">
-                {proyecto.titulo}
-            </h1>
-        </header>
+  <div class="contenedor-1">
+    <header class="Proyecto">
+      <a class="Regresar" href="/">
+        <Boton>
+          <i class="fa fa-arrow-left" />
+          <span>Volver</span>
+        </Boton>
+      </a>
+      <h1 class="Titulo">{proyecto.titulo}</h1>
+    </header>
 
-        <section class="Contenido">
-            {proyecto.contenido}
-        </section>
+    <section class="Contenido">{proyecto.contenido}</section>
+  </div>
+
+  <div class="contenedor-2">
+
+    <div>
+      {#if !!proyecto && !!proyecto.image}
+        <img
+          class="Imagen"
+          src={`${proyecto.image.url}`}
+          alt="img"
+          transition:fade />
+      {/if}
     </div>
-
-    <div class="contenedor-2">
-    
-        <div>
-            {#if !!proyecto && !!proyecto.image}
-                <img class="Imagen" src={`${proyecto.image.url}`} alt="img" transition:fade/>
-            {/if}
-        </div>
-        <footer>            
-            <Boton redondo>
-                Conocer Proyecto
-            </Boton>
-        </footer>
-    </div>
-
+    <footer>
+      <Boton redondo>Conocer Proyecto</Boton>
+    </footer>
+  </div>
 
 </article>
+
+<ProyectosSimilares />
 
 <!-- <section class="SubProyectos">
     
@@ -210,4 +194,4 @@
     </ul>
 </section> -->
 
-<Pie/>
+<Pie />
