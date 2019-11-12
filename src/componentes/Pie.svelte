9ab26ed0266269e53d56cf74beaff2cb11d51406
josecaos <jazzvoon@gmail.com>
2019-11-12 17:24:38 -0600

    FIx  footer que aparece en el cargado del sitio

--- src/componentes/Pie.svelte
+++ src/componentes/Pie.svelte
@@ -1,64 +1,69 @@
 <script>
-import { fade } from "svelte/transition"
-    let logocultura = 'cultura.svg';
-    let logoccd = 'ccd.svg';
-    let logoinali = 'inali.png';
-   	
-    const textoFooter = 'Los lenguajes de la tecnología suelen estar homogeneizados y marcados por las agendas del norte global y del mercado. Estos proyectos descentralizan el uso de las herramientas tecnológicas desde sus concepciones propias, representan la diversidad lingüística cultural y crean un nuevo mapa de términos, vocablos, etiquetas, categorías y reescrituras desde alternativas y apropiaciones.';
+  import { onMount } from "svelte";
+  let logocultura = "cultura.svg";
+  let logoccd = "ccd.svg";
+  let logoinali = "inali.png";
+
+  const textoFooter =
+    "Los lenguajes de la tecnología suelen estar homogeneizados y marcados por las agendas del norte global y del mercado. Estos proyectos descentralizan el uso de las herramientas tecnológicas desde sus concepciones propias, representan la diversidad lingüística cultural y crean un nuevo mapa de términos, vocablos, etiquetas, categorías y reescrituras desde alternativas y apropiaciones.";
+  let footer;
+
+  onMount(async () => {
+    
+    setTimeout(() => {
+        footer.style.opacity = 1;
+    });
+  
+  });
 </script>
 
 <style>
-
-.Pie {
+  .Pie {
+    transition: 1s;
     width: 100%;
     /*padding: 2rem;*/
-    background-color: #F0F0F0;
+    background-color: #f0f0f0;
     display: none;
-    grid-template-columns: 70% 30%;    
-}
-
-@media screen and (min-width: 768px) {
-  .Pie {
-    display: grid;
+    grid-template-columns: 70% 30%;
+    opacity: 0;
   }
-}
 
-.Texto  {
-    
+  @media screen and (min-width: 768px) {
+    .Pie {
+      display: grid;
+    }
+  }
+
+  .Texto {
     font-size: 0.8rem;
     padding: 2rem;
     border-right-style: solid;
     border-right-color: #c4c4c4;
     border-right-width: 1px;
-}
+  }
 
-.Logos {
-    text-align: center;	
+  .Logos {
+    text-align: center;
     display: grid;
     grid-template-columns: 1fr;
     padding: 2rem;
-   
-}
+  }
 
-.Logos img {
-    max-width: 140px;   
-}
-
-
+  .Logos img {
+    max-width: 140px;
+  }
 </style>
 
-<footer class="Pie" transition:fade={{duration:1000}}>
-    <section class="Texto">
-        <p>
-        {textoFooter}
-        </p>
-    
-    </section>
+<footer class="Pie" bind:this={footer}>
+  <section class="Texto">
+    <p>{textoFooter}</p>
 
-    <section class="Logos">
-        <ul>
-            <img src="{logocultura}"  alt=" cultura">
-            <img src="{logoccd}"  alt="ccd">  
-        </ul>
-    </section>
+  </section>
+
+  <section class="Logos">
+    <ul>
+      <img src={logocultura} alt=" cultura" />
+      <img src={logoccd} alt="ccd" />
+    </ul>
+  </section>
 </footer>
